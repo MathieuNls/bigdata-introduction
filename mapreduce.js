68c86e3aa0e24c527bf31d85e883737a61b6b969
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-06-01 12:40:59 -0400

    fix sort

--- mapreduce.js
+++ mapreduce.js
@@ -47,7 +47,13 @@ doctors = doctors.filter(function(doctor){
     yearsPlayed : doctor.end - doctor.begin +1
   }
 }).sort(function(a, b){
-  return a - b;
+  if(a < b){
+    return -1;
+  }else if(a > b){
+    return 1;
+  }else {
+    return 0;
+  }
 });
 
 var totalYear = doctors.reduce(function(total, doctor){
